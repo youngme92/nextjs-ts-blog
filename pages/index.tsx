944af4dc33552ecd270fd50be07ae0558870688f
youngme <youngme992@gmail.com>
2023-04-18 16:57:34 +0900

    hide newsletter update Blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,9 +95,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         </div>
       )}
       {siteMetadata.newsletter.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
-        </div>
+        <div className="flex items-center justify-center pt-4">{/* <NewsletterForm /> */}</div>
       )}
     </>
   )
