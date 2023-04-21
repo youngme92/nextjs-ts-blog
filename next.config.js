e243ad701da0c5a332e38b135ed9597b2b141430
youngme <youngme992@gmail.com>
2023-04-21 11:53:17 +0900

    giscus csp 설정 추가

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com;
-  style-src 'self' 'unsafe-inline';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com giscus.app;
+  style-src 'self' 'unsafe-inline' giscus.app;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
