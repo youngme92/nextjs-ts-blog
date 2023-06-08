8fbb93862a504abe2d921c44a4163a0b4f911367
youngme <youngme992@gmail.com>
2023-06-08 18:29:21 +0900

    install storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'next',
     'next/core-web-vitals',
+    'plugin:storybook/recommended',
   ],
   rules: {
     'prettier/prettier': 'error',
